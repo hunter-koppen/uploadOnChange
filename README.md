883ce31e31be93436114879cabe701a543fd62c2
Hunter Koppen <hunter.koppen@kobeon.nl>
2026-08-18 21:40:58 +0200

    readme etc

--- README.md
+++ README.md
@@ -4,8 +4,5 @@ Add an on change Action (Micro/Nanoflow) to the standard Mendix file and image u
 ## Usage
 Simply add the widget above or below the file/image upload widget from Mendix and configure the action you want to trigger. It will try to find the closest file/image uploader and add the action to it.
 
-## Demo project
-https://uploadonchangewidget-sandbox.mxapps.io/
-
 ## Issues, suggestions and feature requests
 https://github.com/hunter-koppen/uploadOnChange/issues
--- src/UploadOnChange.jsx
+++ src/UploadOnChange.jsx
@@ -1,16 +1,69 @@
 import React, { Component } from "react";
 
+// If the uploader is not in the DOM yet we watch the parent until it shows up,
+// then warn if it never does so a misplaced widget is not silently inert.
+const NOT_FOUND_WARNING_MS = 5000;
+
 export default class UploadOnChange extends Component {
     nodeRef = React.createRef();
+    uploader = null;
+    observer = null;
+    warnTimer = null;
 
     componentDidMount() {
-        setTimeout(() => {
-            // short timeout to make sure all the upload widgets are available on the page
-            const parentNode = this.nodeRef.current.parentNode;
-            const uploader = parentNode.querySelector("input[type=file]");
+        if (this.attach()) {
+            return;
+        }
 
-            uploader.onchange = this.onChange;
-        }, 200);
+        const parentNode = this.nodeRef.current?.parentNode;
+        if (!parentNode) {
+            return;
+        }
+
+        this.observer = new MutationObserver(() => {
+            if (this.attach()) {
+                this.stopWatching();
+            }
+        });
+        this.observer.observe(parentNode, { childList: true, subtree: true });
+
+        this.warnTimer = setTimeout(() => {
+            this.warnTimer = null;
+            console.warn(
+                "UploadOnChange: no file input found in the parent container. " +
+                    "Place the widget in the same container as the file/image upload widget."
+            );
+        }, NOT_FOUND_WARNING_MS);
+    }
+
+    componentWillUnmount() {
+        this.stopWatching();
+        if (this.uploader) {
+            this.uploader.removeEventListener("change", this.onChange);
+            this.uploader = null;
+        }
+    }
+
+    attach() {
+        const uploader = this.nodeRef.current?.parentNode?.querySelector("input[type=file]");
+        if (!uploader) {
+            return false;
+        }
+
+        this.uploader = uploader;
+        uploader.addEventListener("change", this.onChange);
+        return true;
+    }
+
+    stopWatching() {
+        if (this.observer) {
+            this.observer.disconnect();
+            this.observer = null;
+        }
+        if (this.warnTimer !== null) {
+            clearTimeout(this.warnTimer);
+            this.warnTimer = null;
+        }
     }
 
     render() {
@@ -18,8 +71,9 @@ export default class UploadOnChange extends Component {
     }
 
     onChange = () => {
-        if (this.props.onChangeAction) {
-            this.props.onChangeAction.execute();
+        const action = this.props.onChangeAction;
+        if (action?.canExecute && !action.isExecuting) {
+            action.execute();
         }
     };
 }
